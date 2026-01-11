4c12af4471b2965d42062fdc8d21cf35b3322b6d
saket <saketkunwar2005@gmail.com>
2026-01-11 10:59:41 +0545

    test layerdeep

--- c1/code/test.sh
+++ c1/code/test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-./test_arm64 --raw-dir /data/test --model models/best_672x544_fp32_optim.onnx  --input-res 1296 --tile-size 672x544 --solution-out sol13-test-672.csv --limit-craters 13  --no-ranker --all
+#./test_arm64 --raw-dir /data/test --model models/best_672x544_fp32_optim.onnx  --input-res 1296 --tile-size 672x544 --solution-out sol13-test-672.csv --limit-craters 13  --no-ranker --all
 #./test --raw-dir $1 --model models/n/best_672x544.onnx  --input-res 1296 --tile-size 672x544 --solution-out valsol12-672.csv --limit-craters 12
 #./test --raw-dir /data/test/ --model models/best_672x544.onnx --input-res 1296 --tile-size 672x544 --limit-craters 12 --limit-images 50 --solution-out sol_orig.csv --all
+
+./test --raw-dir /data/test --model models/layerdeep.onnx  --input-res 1296 --tile-size 672x544 --solution-out sol13-test-672.csv --limit-craters 11  --no-ranker --all --limit-images 50
